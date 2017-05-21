2c7011118fb402bfec240eff2902fd17bb658e79
nicoarnaise <nicolas.arnaise@telecom-st-etienne.fr>
2017-05-21 11:00:19 -0400

    add note to the scene
    - add a NoteSupport to the scene and set its Y-Rotation and Y-Translate to set the note position
    /!\ DONT TOUCH THE NOTE TRANSFORM !
    - set the note sound by adding it from the asset list
    - play a note by checking "play" while in game or by calling the Note's "Play()" function

---
+++ MusicLabVR/Assets/NoteScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScript : MonoBehaviour {
+
+    public Material idleMaterial;
+    public Material playMaterial;
+    public bool play = false;
+
+    private AudioSource audioS;
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        audioS = transform.GetComponent<AudioSource>();
+        rend = transform.GetComponent<Renderer>();
+	}
+
+    void Update()
+    {
+        if (play)
+        {
+            Play();
+            play = false;
+        }
+        if (!audioS.isPlaying)
+        {
+            rend.material = idleMaterial;
+        }
+        else
+        {
+            rend.material = playMaterial;
+        }
+    }
+
+    public void Play(){
+        audioS.Play();
+    }
+
+
+}
