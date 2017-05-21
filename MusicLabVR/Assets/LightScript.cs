f09d466aad0d7c3b38b7d583c30aa6a84e6bd965
nicoarnaise <nicolas.arnaise@telecom-st-etienne.fr>
2017-05-21 09:27:19 -0400

    custom scale with good light
    put a child to SceneObjects to scale it with the room
    change the scale in run by using Settings gameObject

---
+++ MusicLabVR/Assets/LightScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    private Light l;
+
+	// Use this for initialization
+	void Start () {
+        l = transform.GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.parent.localScale.x * 10 != l.range)
+        {
+            l.range = transform.parent.localScale.x * 10;
+        }
+	}
+}
---
+++ MusicLabVR/Assets/SettingsScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScript : MonoBehaviour {
+
+    public float scale;
+	
+	// Update is called once per frame
+	void Update () {
+        if (scale <= 0) scale = 1;
+		if(!transform.parent.localScale.Equals(new Vector3(scale,scale, scale)))
+        {
+            transform.parent.localScale = new Vector3(scale, scale, scale);
+        }
+	}
+}
