4a12d7cd8244b8cfa1fe49a8b105cb6dc8f366f2
nicoarnaise <nicolas.arnaise@telecom-st-etienne.fr>
2017-06-07 20:17:52 -0400

    Add buttons but not usable for now

---
+++ MusicLabVR/Assets/NoteTimeLineScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteTimeLineScript : MonoBehaviour {
+
+    public bool activate = false;
+    public int menuZ = 10;
+    public GameObject menuPref;
+    public bool hasMenuOpened = false;
+    public int partitionIndex = 0;
+
+    private GameObject childMenu;
+
+    // Update is called once per frame
+    void Update () {
+		if (activate)
+        {
+            activate = false;
+            openMenu();
+        }
+
+        if (childMenu)
+            hasMenuOpened = true;
+        else
+            hasMenuOpened = false;
+	}
+
+    public void openMenu()
+    {
+        if (transform.parent.GetComponentInChildren<MenuPrefScript>())
+            Destroy(transform.parent.GetComponentInChildren<MenuPrefScript>().transform.parent.gameObject);
+
+        if (hasMenuOpened)
+            Destroy(childMenu);
+        else
+        {
+            childMenu = Instantiate(menuPref, transform.parent, false);
+            childMenu.transform.localPosition = new Vector3(transform.localPosition.x, 0.003f, menuZ);
+        }
+    }
+}
--- MusicLabVR/Assets/TimeLineScript.cs
+++ MusicLabVR/Assets/TimeLineScript.cs
@@ -26,7 +26,7 @@ public class TimeLineScript : MonoBehaviour {
     
 
     // list Note and their duration
-    public List<KeyValuePair<NoteScript, float>> partition;
+    public List<Note> partition;
 
     // list NoteTimeLine
     private List<Page> pages;
@@ -44,7 +44,7 @@ public class TimeLineScript : MonoBehaviour {
             Z1 = 0;
             Z2 = 0;
         }
-        partition = new List<KeyValuePair<NoteScript, float>>();
+        partition = new List<Note>();
         pages = new List<Page>();
         // creation of the first page
         pages.Add(new Page());
@@ -63,15 +63,18 @@ public class TimeLineScript : MonoBehaviour {
     {
         if(pToShow != pageShown && pToShow < pages.Count && pToShow > -1)
         {
+            if (GetComponentInChildren<MenuPrefScript>().gameObject)
+                Destroy(GetComponentInChildren<MenuPrefScript>().transform.parent.gameObject);
+
             pageToShow = pToShow;
-            foreach(GameObject NoteTimeLine in pages[pageShown].content)
+            foreach(Note note in pages[pageShown].content)
             {
-                NoteTimeLine.SetActive(false);
+                note.NoteTimeLine.SetActive(false);
             }
             pageShown = pageToShow;
-            foreach (GameObject NoteTimeLine in pages[pageShown].content)
+            foreach (Note note in pages[pageShown].content)
             {
-                NoteTimeLine.SetActive(true);
+                note.NoteTimeLine.SetActive(true);
             }
             nbFourth = pages[pageShown].nbFourth;
         }
@@ -83,32 +86,33 @@ public class TimeLineScript : MonoBehaviour {
         {
             showPage(pages.Count - 1);
 
-            GameObject newNote = Instantiate(NoteTimeLineRef, transform, false);
-            newNote.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos,Z1);
-            newNote.transform.localScale = new Vector3(newNote.transform.localScale.x * (selected.value * 4), newNote.transform.localScale.y, newNote.transform.localScale.z);
+            GameObject newNoteGO = Instantiate(NoteTimeLineRef, transform, false);
+            newNoteGO.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos,Z1);
+            newNoteGO.transform.localScale = new Vector3(newNoteGO.transform.localScale.x * (selected.value * 4), newNoteGO.transform.localScale.y, newNoteGO.transform.localScale.z);
             if(addon != "")
-                newNote.GetComponentInChildren<TextMesh>().text = "" + Octave + "\n" + addon;
+                newNoteGO.GetComponentInChildren<TextMesh>().text = "" + Octave + "\n" + addon;
             else
-                newNote.GetComponentInChildren<TextMesh>().text = "" + Octave;
-            newNote.GetComponent<Renderer>().material = Note.idleMaterial;
+                newNoteGO.GetComponentInChildren<TextMesh>().text = "" + Octave;
+            newNoteGO.GetComponent<Renderer>().material = Note.idleMaterial;
 
             if(nbFourth + selected.value * 4 > nbFourthMax)
             {
-                foreach(GameObject NoteTimeLine in pages[pages.Count - 1].content)
+                foreach(Note note in pages[pages.Count - 1].content)
                 {
-                    NoteTimeLine.SetActive(false);
+                    note.NoteTimeLine.SetActive(false);
                 }
                 // add new page
                 pages.Add(new Page());
                 nbFourth = 0;
                 pageToShow++;
                 pageShown++;
-                newNote.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos, Z1);
+                newNoteGO.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos, Z1);
             }
-            pages[pages.Count - 1].content.Add(newNote);
-            nbFourth += (int)(selected.value * 4);
-            pages[pages.Count - 1].nbFourth = nbFourth;
-            partition.Add(new KeyValuePair<NoteScript, float>(Note, selected.value));
+            Note newNote = new Note(newNoteGO, Note, pageShown, pages[pageShown].content.Count - 1, partition.Count - 1, (int)(selected.value * 4));
+            pages[pageShown].content.Add(newNote);
+            nbFourth += newNote.nbFourth;
+            pages[pageShown].nbFourth = nbFourth;
+            partition.Add(newNote);
         }   
     }
 
@@ -118,29 +122,45 @@ public class TimeLineScript : MonoBehaviour {
         {
             showPage(pages.Count - 1);
 
-            GameObject newNote = Instantiate(NoteTimeLineRef, transform, false);
-            newNote.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos, Z1);
-            newNote.transform.localScale = new Vector3(newNote.transform.localScale.x * (selected.value * 4), newNote.transform.localScale.y, newNote.transform.localScale.z);
-            newNote.GetComponentInChildren<TextMesh>().text = "";
+            GameObject newNoteGO = Instantiate(NoteTimeLineRef, transform, false);
+            newNoteGO.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos, Z1);
+            newNoteGO.transform.localScale = new Vector3(newNoteGO.transform.localScale.x * (selected.value * 4), newNoteGO.transform.localScale.y, newNoteGO.transform.localScale.z);
+            newNoteGO.GetComponentInChildren<TextMesh>().text = "";
 
             if (nbFourth + selected.value * 4 > nbFourthMax)
             {
-                foreach (GameObject NoteTimeLine in pages[pages.Count - 1].content)
+                foreach (Note note in pages[pages.Count - 1].content)
                 {
-                    NoteTimeLine.SetActive(false);
+                    note.NoteTimeLine.SetActive(false);
                 }
                 // add new page
                 pages.Add(new Page());
                 nbFourth = 0;
                 pageToShow++;
                 pageShown++;
-                newNote.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos, Z1);
+                newNoteGO.transform.localPosition = new Vector3(startX + spaceX * (nbFourth + (selected.value * 2) - 1), Ypos, Z1);
             }
+            Note newNote = new Note(newNoteGO, null, pageShown, pages[pageShown].content.Count - 1, partition.Count - 1, (int)(selected.value * 4));
             pages[pages.Count - 1].content.Add(newNote);
             nbFourth += (int)(selected.value * 4);
             pages[pages.Count - 1].nbFourth = nbFourth;
-            partition.Add(new KeyValuePair<NoteScript, float>(null, selected.value));
+            partition.Add(newNote);
         }
+    }
+
+    public void AddAt(int page, float x, Note note)
+    {
+        int size = (int)(note.NoteTimeLine.transform.localScale.x / (0.002f));
+        showPage(page);
+        pages[page].nbFourth += size;
+        int index = 0;
+        while(pages[page].content[index].NoteTimeLine.transform.localPosition.x < x)
+        {
+            index++;
+        }
+        pages[page].content.Insert(index, note);
+        partition.Insert(note.indexInPartition, note);
+        // à finir
     }
 
     public void Play()
@@ -156,10 +176,10 @@ public class TimeLineScript : MonoBehaviour {
     {
         if (noteIndex < partition.Count)
         {
-            float clipDuration = partition[noteIndex].Value;
+            float clipDuration = partition[noteIndex].nbFourth * 0.25f;
 
-            if(partition[noteIndex].Key)
-                partition[noteIndex].Key.PlayT(1.2f * clipDuration);
+            if(partition[noteIndex].NoteToPlay)
+                partition[noteIndex].NoteToPlay.PlayT(1.2f * clipDuration);
 
             float errorTime = Time.time - startTime - currentTime;
 
@@ -172,16 +192,67 @@ public class TimeLineScript : MonoBehaviour {
             StartCoroutine(playMusic());
         }
     }
+
+    public void remove (NoteTimeLineScript toRemove, bool destroy)
+    {
+        Note noteToRemove = partition[toRemove.partitionIndex];
+        int size = noteToRemove.nbFourth;
+        float tRx = spaceX * size;
+        partition.RemoveAt(toRemove.partitionIndex);
+        pages[pageShown].content.Remove(noteToRemove);
+        foreach (Note note in pages[pageShown].content)
+        {
+            if (note.NoteTimeLine.transform.localPosition.x > toRemove.transform.localPosition.x)
+                note.NoteTimeLine.transform.localPosition = new Vector3(note.NoteTimeLine.transform.localPosition.x - tRx, note.NoteTimeLine.transform.localPosition.y, note.NoteTimeLine.transform.localPosition.z);
+        }
+        pages[pageShown].nbFourth -= size;
+        nbFourth -= size;
+        if (pageShown < pages.Count - 1)
+        {
+            while(pages[pageShown+1].content[0].NoteTimeLine.transform.localScale.x / (0.002f) <= nbFourthMax - nbFourth )
+            {
+                int sizeNext = (int)(pages[pageShown + 1].content[0].NoteTimeLine.transform.localScale.x / (0.002f));
+                AddAt(pageShown, spaceX * (nbFourth + (sizeNext * 2) - 1), pages[pageShown + 1].content[0]);
+                pages[pageShown].content.Add(pages[pageShown + 1].content[0]);
+                pages[pageShown].nbFourth += sizeNext;
+                showPage(pageShown + 1);
+                remove(pages[pageShown].content[0].NoteTimeLine.GetComponent<NoteTimeLineScript>(), false);
+                showPage(pageShown - 1);
+            }
+        }
+        if(destroy)
+            Destroy(toRemove.transform.gameObject);
+    }
 }
 
 public class Page
 {
-    public List<GameObject> content;
+    public List<Note> content;
     public int nbFourth;
 
     public Page()
     {
-        content = new List<GameObject>();
+        content = new List<Note>();
         nbFourth = 0;
+    }
+}
+
+public class Note
+{
+    public GameObject NoteTimeLine;
+    public NoteScript NoteToPlay;
+    public int page;
+    public int indexInPage;
+    public int indexInPartition;
+    public int nbFourth;
+
+    public Note(GameObject _NoteTimeLine, NoteScript _NoteToPlay, int _page, int _indexInPage, int _indexInPartition, int _nbFourth)
+    {
+        NoteTimeLine = _NoteTimeLine;
+        NoteToPlay = _NoteToPlay;
+        page = _page;
+        indexInPage = _indexInPage;
+        indexInPartition = _indexInPartition;
+        nbFourth = _nbFourth;
     }
 }
